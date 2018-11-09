6bb761529a3ce9def8e501639b69e61c637cd78f
Sonchucks <son.hyun.uk@hotmail.com>
2018-11-09 16:41:14 +0000

    Did some styling as well as added delete functionality for reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -10,6 +10,14 @@ class ReviewsController < ApplicationController
       redirect_to :back
     end
   end
+
+  def destroy
+    @product = Product.find_by(id: params[:product_id])
+    @review = @product.reviews.find(params[:id])
+    @review.destroy
+    redirect_to product_path(@product)
+  end
+
   private
     def review_params
       params.require(:review).permit(
