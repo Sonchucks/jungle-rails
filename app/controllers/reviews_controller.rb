d9e512d1af00dbaad5f6224b6f4e13decb37267c
Sonchucks <son.hyun.uk@hotmail.com>
2018-11-09 00:31:51 +0000

    Can now add reviews when logged in

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,20 @@
 class ReviewsController < ApplicationController
+  def create
+    @product = Product.find_by(id: params[:product_id])
+    @review = @product.reviews.new(review_params)
+    @review.user_id = session[:user_id]
+    if @review.save!
+      redirect_to :back
+    else
+      @status = true
+      redirect_to :back
+    end
+  end
+  private
+    def review_params
+      params.require(:review).permit(
+        :description,
+        :rating
+      )
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,7 @@ Rails.application.routes.draw do
   root to: 'products#index'
 
   resources :products, only: [:index, :show] do
-    resources :reviews, only: [:show] do
-      post   :add_comment
-    end
+    resources :reviews, only: [:create]
   end
   resources :categories, only: [:show]
 
