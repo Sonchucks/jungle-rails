4cbaf8ada9c1e30702f6f590fbf2cbe31bd71198
Sonchucks <son.hyun.uk@hotmail.com>
2018-11-13 16:39:22 +0000

    Setup add to card spec

---
+++ spec/features/add_to_cart_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.feature "AddToCarts", type: :feature, js: true do
+ before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+  scenario "They add a product to cart" do
+    # ACT
+    visit root_path
+    click_on("Add", match: :first)
+    # DEBUG / VERIFY
+    save_screenshot
+    within('.navbar') { expect(page).to have_content('My Cart (1)') }
+  end
+end
